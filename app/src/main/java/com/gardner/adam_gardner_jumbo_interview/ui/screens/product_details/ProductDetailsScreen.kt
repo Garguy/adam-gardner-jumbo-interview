c671c9c429bb586cb557e1d507aac592577abb16
Adam Gardner <gardner.r.adam@gmail.com>
2023-04-17 16:03:59 -0500

    Same addition to Details with image request giving back 403. If time allows look into 403.

--- app/src/main/java/com/gardner/adam_gardner_jumbo_interview/ui/screens/product_details/ProductDetailsScreen.kt
+++ app/src/main/java/com/gardner/adam_gardner_jumbo_interview/ui/screens/product_details/ProductDetailsScreen.kt
@@ -1,15 +1,19 @@
 package com.gardner.adam_gardner_jumbo_interview.ui.screens.product_details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,9 +25,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
+import com.gardner.adam_gardner_jumbo_interview.R
 import com.gardner.adam_gardner_jumbo_interview.data.product.ProductViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -55,10 +63,21 @@ fun ProductDetailsScreen(
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
-                AsyncImage(
-                    model = product?.imageInfo?.primaryView?.get(1)?.url ?: "https://via.placeholder.com/150",
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
+                SubcomposeAsyncImage(
+                    model = product?.imageInfo?.primaryView?.firstOrNull()?.url,
+                    contentDescription = product?.title,
+                    modifier = Modifier
+                        .size(96.dp)
+                        .padding(16.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop,
+                    loading = { CircularProgressIndicator() },
+                    error = {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = "An error occurred loading image"
+                        )
+                    }
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(
